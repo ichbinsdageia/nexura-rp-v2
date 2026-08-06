d85b2c162c192d5c06ec5d9f8c2ef12b22559353
ichbinsdageia <ichbinsdageia@gmail.com>
2026-08-06 12:36:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 import { bindRouter, renderRoute } from './router.js';
+import { finishOAuthCallback } from './lib/supabase.js';
+
+try {
+  await finishOAuthCallback();
+} catch (error) {
+  console.error('Discord-OAuth-Callback fehlgeschlagen:', error);
+}
 
 bindRouter();
-renderRoute();
+await renderRoute();
