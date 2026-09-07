dc8b9bc9da2fd71508957378892e33d1d19a3b84
ichbinsdageia <ichbinsdageia@gmail.com>
2026-09-07 23:24:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-import { bindRouter, renderRoute } from './router.js?v=20260814-2';
+import {
+  bindRouter,
+  renderRoute,
+} from './router.js?v=20260907-legal';
 
+
+/* =========================================================
+   NEXURA WEBSITE START
+========================================================= */
+
+/*
+  Router aktivieren:
+  - interne Navigation
+  - Browser Vor/Zurück
+*/
 bindRouter();
+
+
+/*
+  Aktuelle Seite laden.
+*/
 renderRoute();
